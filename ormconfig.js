d5c4b92cbcee99a84ec7386f7c2d46a4662f7a78
calm1205 <naginani072@gmail.com>
2022-04-21 09:15:06 +0900

    version down typeorm

---
+++ ormconfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5555,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+  autoLoadEntities: true,
+
+  entities: ['dist/entities/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migrations',
+  },
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ItemsModule } from './item/item.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import typeormConfig from './config/typeorm.config';
 import graphqlConfig from './config/graphql.config';
 
 @Module({
   imports: [
     ItemsModule,
-    TypeOrmModule.forRoot(typeormConfig),
+    TypeOrmModule.forRoot(),
     GraphQLModule.forRootAsync(graphqlConfig),
   ],
   controllers: [],
